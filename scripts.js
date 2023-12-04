39ca8fe1d55c24576f5aefd7128c0c98649026ca
testrsp <151908514+testrsp@users.noreply.github.com>
2023-12-04 14:33:42 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -69,6 +69,8 @@ function generateNavigationLinks(navDataFile) {
 function toggleNavigationBar() {
    const navigation = document.getElementById('navigation');
     navigation.style.display = (window.getComputedStyle(navigation).display === 'none') ? 'block' : 'none';
+    navigation.style.display = (screenWidth <= 600) ? 'none' : 'block';
+
     //navigation.style.visibility = (window.getComputedStyle(navigation).visibility === 'hidden') ? 'visible' : 'hidden';
 }
 // Update header elements based on the current language
